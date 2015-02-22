4195fae33ca06e71584710b8e6aeec3dbf1d9d9c
quinn-madson <quinn.madson@gmail.com>
2015-02-21 19:43:58 -0600

    initial project commit.

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# tiny-rider-light-bridge
-Arduino-based light bridge and control circuit for 1:32 scale slotcar track using Ultimate Racer RMS.
+# Tiny Rider Light Bridge and Control Circuit #
+
+## What is this? ##
+
+Code and diagrams to build an Arduino-based infrared light bridge and relay circuits for 1:32 scale slotcar track using the Ultimate Racer RMS. This is was used to build a 4 lane track for the Tiny Riders in Milwaukee, WI, USA. Code, diagrams, and information can be adapted for different scenarios, number of lanes, etc.
+
+## Project details ##
+
+Check the arduino-code folder for the source. You will also need to use the UnoJoy project so that the Arduino appears as USB HID-compliant device. The Ultimate Racer RMS software can only interface in a limited number of ways. We didn't want to use parallel port or serial port methods asa they proved problematic in the past and usually required antiquated hardware. The Arduino will appear as a PS3 stlye gamepad to the computer to send race events to Ultimate Racer. Getting events out or Ultimate Racer is a bit challenging because few interfaces are supported. To get around this problem, the Arduino controls the open/closed state of the relay circuit and only sends signals into Ultimate Racer to pause the heat if a car gets deslotted. The Arduino is connected to a 5v relay (shown as 3.3 in diagram -- either is fine and supported by the Arduino). The 5v relay then triggers 2 12v relays to control power to the lanes.
+
+![Track Diagram](https://github.com/quinn-madson/tiny-rider-light-bridge/blob/master/images/track-diagram.jpg?raw=true)
+
+We used 2 power supplies each running at 14v each powering 2 lanes in the configuration. Make sure to put fuses  (we used 4 amp) or thermal protection on the circuit or you can damage your controllers.
+
+![Infrared Diagram](https://github.com/quinn-madson/tiny-rider-light-bridge/blob/master/images/infrared-circuit.jpg?raw=true)
+
+We use an infrared beam from a bridge to detect when each lane has completed a lap.
+
+## Construction ##
+
+![Construction 1](https://github.com/quinn-madson/tiny-rider-light-bridge/blob/master/images/bridge-construction1.jpg?raw=true)
+
+![Construction 2](https://github.com/quinn-madson/tiny-rider-light-bridge/blob/master/images/bridge-construction2.jpg?raw=true)
+
+![Construction 3](https://github.com/quinn-madson/tiny-rider-light-bridge/blob/master/images/bridge-construction3.jpg?raw=true)
+
+![Construction 4](https://github.com/quinn-madson/tiny-rider-light-bridge/blob/master/images/bridge-construction4.jpg?raw=true)
+
+![Relays](https://github.com/quinn-madson/tiny-rider-light-bridge/blob/master/images/12vRelays.jpg?raw=true)
+
+![Power Taps](https://github.com/quinn-madson/tiny-rider-light-bridge/blob/master/images/power-taps.jpg?raw=true)
+
---
+++ arduino-code/tiny-rider-light-bridge.ino
@@ -0,0 +1,156 @@
+#include "UnoJoy.h"
+
+const int redLane = 2;
+const int yellowLane = 3;
+const int greenLane = 4;
+const int blueLane = 5;
+const int killSwitch = 6;
+const int lilRelay = 7;
+const int statusLed = 13;
+
+int redBroken = 0;
+int yellowBroken = 0;
+int greenBroken = 0;
+int blueBroken = 0;
+int relayOpen = 1;
+int redMemory = 0;
+int yellowMemory = 0;
+int greenMemory = 0;
+int blueMemory = 0;
+
+void setup() {
+  //Serial.begin(9600);
+  pinMode(redLane, INPUT);
+  pinMode(yellowLane, INPUT);
+  pinMode(greenLane, INPUT);
+  pinMode(blueLane, INPUT);
+  pinMode(killSwitch, INPUT);
+  pinMode(lilRelay, OUTPUT);
+  pinMode(statusLed, OUTPUT); 
+
+  digitalWrite(killSwitch, HIGH);
+  digitalWrite(lilRelay, LOW);
+
+  blink();
+  blink();
+  blink();
+  blink();
+  
+  setupUnoJoy();
+  
+}
+
+void loop() {
+  //delay(20);
+  beamEvents();
+  killEvent();
+  //joystickTest();
+}
+
+void toggleRelay() {
+  dataForController_t controllerData = getBlankDataForController();
+  controllerData.startOn = 1;
+  setControllerData(controllerData);
+  
+  if (relayOpen == 1) {
+    relayOpen = 0;
+    digitalWrite(lilRelay, HIGH);
+  } else {
+    relayOpen = 1;
+   digitalWrite(lilRelay, LOW); 
+  }
+  delay(250);
+}  
+
+void killEvent() {
+  int killStatus = digitalRead(killSwitch);
+  //Serial.println(killStatus);
+  if (killStatus == 0) {
+    toggleRelay(); 
+  }
+}
+
+void joystickTest() {
+   
+   dataForController_t controllerData = getBlankDataForController();
+   controllerData.triangleOn = 1;
+   controllerData.circleOn = 1;
+   controllerData.squareOn = 1;
+   controllerData.crossOn = 1;
+   setControllerData(controllerData);
+   delay(500);
+  
+   dataForController_t controllerData1 = getBlankDataForController();
+   controllerData1.triangleOn = 1;
+   setControllerData(controllerData1);
+   delay(500);
+   
+   dataForController_t controllerData2 = getBlankDataForController();
+   controllerData2.circleOn = 1;
+   setControllerData(controllerData2);
+   delay(500);
+   
+   dataForController_t controllerData3 = getBlankDataForController();
+   controllerData3.squareOn = 1;
+   setControllerData(controllerData3);
+   delay(500);
+   
+   dataForController_t controllerData4 = getBlankDataForController();
+   controllerData4.crossOn = 1;
+   setControllerData(controllerData4);
+   delay(500);
+}
+
+void blink() {
+  digitalWrite(statusLed, HIGH);
+  delay(500);
+  digitalWrite(statusLed, LOW);
+  delay(500);
+}
+
+void beamEvents() {
+  redBroken = digitalRead(redLane); 
+  yellowBroken = digitalRead(yellowLane); 
+  greenBroken = digitalRead(greenLane); 
+  blueBroken = digitalRead(blueLane); 
+
+  dataForController_t controllerData = getBlankDataForController();
+
+  if(redBroken == 1 /*&& redMemory == 0*/) {
+    //Serial.println("red: Broken");
+    digitalWrite(statusLed, HIGH);
+    controllerData.triangleOn = 1;
+  } 
+  else if (yellowBroken == 1 /*&& yellowMemory == 0*/) { 
+    //Serial.println("yellow: Broken");
+    digitalWrite(statusLed, HIGH);
+    controllerData.circleOn = 1;
+  } 
+  else if(greenBroken == 1 /* && greenMemory == 0 */) { 
+    //Serial.println("green: Broken");
+    digitalWrite(statusLed, HIGH);
+    controllerData.squareOn = 1;
+  } 
+  else if(blueBroken == 1 /*&& blueMemory == 0*/) { 
+    //Serial.println("blue: Broken");
+    digitalWrite(statusLed, HIGH);
+    controllerData.crossOn = 1;
+  } 
+  else {
+    digitalWrite(statusLed, LOW); 
+  }
+
+  redMemory = redBroken;
+  yellowMemory = yellowBroken;
+  greenMemory = greenBroken;
+  blueMemory = blueBroken;
+
+  setControllerData(controllerData);
+
+}
+
+
+
+
+
+
